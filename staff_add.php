03058fe4e9077481441e22cff9708eaab2896fa4
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-06-12 15:49:27 +0900

    入力画面

--- staff_add.php
+++ staff_add.php
@@ -11,7 +11,15 @@
 スタッフ管理<br>
 <br>
 <form action="staff_add.check" method="post">
-    
+    スタッフ名を入力してください。<br>
+    <input type="text" name="name" style="width:200px"><br>
+    パスワードを入力して下さい。<br>
+    <input type="password" name="password" style="width:100px"><br>
+    パスワードをもう一度入力して下さい。<br>
+    <input type="password" name="password2" style="width:100px"><br>
+    <br>
+    <input type="button" onclick="history.back()" value="戻る">
+    <input type="submit" value="OK">
 </form>
 </body>
 </html>
