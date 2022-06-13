548883ff055bf98724ea52d1e1653ae72128ad2b
Nagai Asuka <87313557+nagaiasuka@users.noreply.github.com>
2022-06-13 14:07:22 +0900

    一覧機能実装

---
+++ staff_list.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    try {
+        // データベース接続
+        $dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = '';
+        $dbh = new PDO($dsn,$user,$password);
+        $dbh ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    
+        // SQL文を使ってレコードを追加
+        $sql = 'SELECT name FROM mst_staff WHERE 1';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+    
+        // データベースからの切断
+        $dbh=null;
+
+        print 'スタッフ一覧<br><br>';
+
+        while(true){
+            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+            if($rec == false){
+                break;
+            }
+            print $rec['name'];
+            print '<br>';
+        }
+    }
+    catch(Exception $e) {
+        print $e->getMessage();
+        print 'ただいま障害をにより大変ご迷惑をおかけしております。';
+        exit();
+    }
+    ?>
+</body>
+</html>
